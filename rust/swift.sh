4274828728dc2ebc1ccf8a635c8c52837931b0d2
John Sohn <john.sohn@gmail.com>
2024-09-07 15:48:20 -0400

    update testing swift.sh script

--- rust/swift.sh
+++ rust/swift.sh
@@ -1,28 +1,22 @@
+# build Rust release mode target for x64 and Arch64
 cargo build --release --lib --target aarch64-apple-darwin
 cargo build --release --lib --target x86_64-apple-darwin
+
+# generate Rust Swift bindings
 cargo run --bin uniffi-bindgen generate src/sguitar.udl --language swift --out-dir swift
 
+# setup bindings into usable paths
 rm -rf swift/include
 mkdir swift/include
-cp swift/sguitar.swift include
 cp swift/sguitarFFI.h swift/include
 cp swift/sguitarFFI.modulemap swift/include/module.modulemap
-cp swift/sguitar.swift swift/include
+rm swift/libsguitar-macos.a
+lipo -create -output swift/libsguitar-macos.a target/aarch64-apple-darwin/release/libsguitar.a target/x86_64-apple-darwin/release/libsguitar.a
 
-#rm -rf libs/libsguitar-macos.a
-#rm -rf libs/libsguitar-maccatalyst.a
-rm -rf swift/libs
-mkdir swift/libs
-lipo -create -output swift/libs/libsguitar-macos.a target/aarch64-apple-darwin/release/libsguitar.a target/x86_64-apple-darwin/release/libsguitar.a
-#lipo -create -output libs/libsguitar-maccatalyst.a \
-#      target/aarch64-apple-ios-macabi/release/libsguitar.a \
-#      target/x86_64-apple-ios-macabi/release/libsguitar.a
+rm -rf swift/SGuitarFFI.xcframework
+xcodebuild -create-xcframework -library swift/libsguitar-macos.a -headers swift/include -output swift/SGuitarFFI.xcframework
 
-rm -rf SGuitarFFI.xcframework
-xcodebuild -create-xcframework -library swift/libs/libsguitar-macos.a -headers swift/include -output SGuitarFFI.xcframework
-
-#cargo build --release
-#cargo run --bin uniffi-bindgen generate src/sguitar.udl --language swift --out-dir swift
+### tagged for deletion...
 #swiftc \
 #    -module-name sguitar \
 #    -emit-library -o libsguitar.a \
@@ -32,11 +26,3 @@ xcodebuild -create-xcframework -library swift/libs/libsguitar-macos.a -headers s
 #    -lsguitar \
 #    -Xcc -fmodule-map-file=./swift/sguitarFFI.modulemap \
 #    swift/sguitar.swift
-#mv libsguitar.a swift
-#mv sguitar.swiftmodule swift
-#mv .swiftdoc swift
-#mv .swiftsourceinfo swift
-
-
-
-
