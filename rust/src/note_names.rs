78a5538ae5298fc57118fb8e11e34ed02ef758e8
John Sohn <john.sohn@gmail.com>
2024-09-14 23:39:19 -0400

    remove Rust experiment from main, reorganize project

--- rust/src/note_names.rs
+++
@@ -1,9 +0,0 @@
-#[allow(dead_code)]
-pub const NOTE_NAMES_SHARP: [&str; 12] = [
-    "C", "C♯", "D", "D♯", "E", "F", "F♯", "G", "G♯", "A", "A♯", "B",
-];
-
-#[allow(dead_code)]
-pub const NOTE_NAMES_FLAT: [&str; 12] = [
-    "C", "D♭", "D", "E♭", "E", "F", "G♭", "G", "A♭", "A", "B♭", "B",
-];
