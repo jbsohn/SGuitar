4ccb2fc2ff865a9a57290aa3d21798084951b356
John Sohn <john.sohn@gmail.com>
2024-10-06 16:49:54 -0400

    added some tests, confirm SQLiteCpp as vcpkg is working

---
+++ cpp/src/tests/TestDB.cpp
@@ -0,0 +1,26 @@
+
+#include <iostream>
+#include <doctest/doctest.h>
+#include <SQLiteCpp/SQLiteCpp.h>
+
+TEST_CASE("Testing DB: SELECT") {
+    const SQLite::Database db("../../db/main.sqlite3");
+
+    std::cout << "SELECT Scales..." << std::endl;
+    SQLite::Statement queryScale(db, "SELECT * FROM scale");
+    while (queryScale.executeStep()) {
+        std::cout << std::format("{}: {}, {}\n",
+            queryScale.getColumn(0).getInt(),
+            queryScale.getColumn(1).getString(),
+            queryScale.getColumn(2).getString());
+    }
+
+    std::cout << "SELECT Chords...";
+    SQLite::Statement queryChord(db, "SELECT * FROM chord");
+    while (queryChord.executeStep()) {
+        std::cout << std::format("{}: {}, {}\n",
+            queryChord.getColumn(0).getInt(),
+            queryChord.getColumn(1).getString(),
+            queryChord.getColumn(2).getString());
+    }
+}
