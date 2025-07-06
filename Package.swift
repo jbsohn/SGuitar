1cde0a80a3f2fe5c72d4e61cd90613143102c6ca
John Sohn <john.sohn@gmail.com>
2025-07-06 17:58:38 -0400

    update Swift package

--- Package.swift
+++ Package.swift
@@ -16,8 +16,8 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "SGuitar",
-            url: "https://github.com/jbsohn/SGuitar/releases/download/v0.0.6/SGuitar.xcframework.zip",
-            checksum: "f9270bae8465a9ea1bf929062d942e096433c30cf04d54ebf2422c381ad10d1e"
+            url: "https://github.com/jbsohn/SGuitar/releases/download/v0.0.12/SGuitar.xcframework.zip",
+            checksum: "4864082ae444e89b6443f7a587b10654e87cc005a6cbff8d93c3a3874f886d94"
         )
     ]
 )
