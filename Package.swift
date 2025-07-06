23aa36117286b1df5e65a62ce958538116cb12c7
John Sohn <john.sohn@gmail.com>
2025-07-05 21:20:39 -0400

    add Package.swift

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// Package.swift
+// swift-tools-version:5.6
+import PackageDescription
+
+let package = Package(
+    name: "SGuitar",
+    platforms: [
+        .iOS(.v14),
+    ],
+    products: [
+        .library(
+            name: "SGuitar",
+            targets: ["SGuitar"]
+        ),
+    ],
+    targets: [
+        .binaryTarget(
+            name: "SGuitar",
+            url: "https://github.com/jbsohn/SGuitar/releases/download/v0.0.5/SGuitar.xcframework.zip",
+            checksum: "f9270bae8465a9ea1bf929062d942e096433c30cf04d54ebf2422c381ad10d1e"
+        )
+    ]
+)
